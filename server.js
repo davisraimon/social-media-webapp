88ed1d695af4d6a4c4da1b779237a8e8b9fee223
Davis Raimon <davisraimon@gmail.com>
2020-08-22 00:38:52 +0530

    basic routing commit

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ const app = express();
 connectDB();
 
 app.get("/", (req, res) => res.send("API Running"));
+
+//Define Routes Used in this Appliction
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Listening to PORT ${PORT}`));
