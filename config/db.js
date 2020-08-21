4a4879328d1000ccd78f8947ff754c92a9bc1da9
Davis Raimon <davisraimon@gmail.com>
2020-08-22 00:16:37 +0530

    DB connection config files commit

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURL");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Mongo DB Connected Successfully!!!");
+  } catch (err) {
+    console.log("err.message");
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+
+// Connect to DB
+connectDB();
+
 app.get("/", (req, res) => res.send("API Running"));
 const PORT = process.env.PORT || 5000;
 
