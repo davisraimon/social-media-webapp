5f8ade2618e63574b8b97bf4683d6568124ebd47
Davis Raimon <davisraimon@gmail.com>
2020-09-01 00:49:57 +0530

    code update commit

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -18,7 +18,7 @@ export default function (state = initialState, action) {
     case GET_POSTS:
       return { ...state, posts: payload, loading: false };
     case ADD_POST:
-      return { ...state, posts: [...state.posts, payload], loading: false };
+      return { ...state, posts: [payload, ...state.posts], loading: false };
     case DELETE_POST:
       return {
         ...state,
