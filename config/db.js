6eea28d8f6f6744b0aa430fe8332f23d0035cf0e
Davis Raimon <davisraimon@gmail.com>
2020-09-02 20:55:51 +0530

    deployment code update

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const config = require("config");
-const db = config.get("mongoURL");
+// const db = config.get("mongoURL");
+const db = process.env.mongoURL;
 
 const connectDB = async () => {
   try {
