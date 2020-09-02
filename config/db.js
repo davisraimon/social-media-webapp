dc6b8509ae50695aaa5e49baed2a615e59cd7453
Davis Raimon <davisraimon@gmail.com>
2020-09-02 21:43:02 +0530

    deployement preparation commit

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
+const de = require("dotenv").config();
 const mongoose = require("mongoose");
 const config = require("config");
-// const db = config.get("mongoURL");
 const db = process.env.mongoURL;
 
 const connectDB = async () => {
